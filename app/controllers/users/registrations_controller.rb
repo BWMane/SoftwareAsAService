a50362dba945a30da3bfeeaf86fe6c170265f81b
Bakang Nthekela <bnthekela@gmail.com>
2021-05-13 16:38:16 +0000

    Added comments for stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  #Extend default devise gem behaviour so that
+  #so that users signing up with Pro account (Plan ID 2)
+  #save with a special stripe subscription function
+  #Otherwise devise signs up user as usual (Plan ID 1)
+  
+  
   def create
     super do |resource|
       if params[:plan]
